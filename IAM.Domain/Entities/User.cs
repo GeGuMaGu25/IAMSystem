830ff456a635cd0e647617fce9406993bb6aebdf
GeGuMaGu25 <gustavoolivares302@gmail.com>
2026-09-03 19:51:15 -0500

    feat(domain): agregar entidad User y contrato IUserRepository

---
+++ IAM.Domain/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿namespace IAM.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string Email { get; private set; } = string.Empty;
+    public string PasswordHash { get; private set; } = string.Empty;
+    public DateTime CreatedAt { get; private set; }
+    
+    // Constructor sin parámetros requerido por Entity Framework Core
+    protected User() { }
+
+    public User(string email, string passwordHash)
+    {
+        if  (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("El email no puede estar vacio", nameof(email));
+        
+        if (string.IsNullOrWhiteSpace(passwordHash))
+            throw new ArgumentException("El hash de la contraseña es requerido",  nameof(passwordHash));
+        
+        Id = Guid.NewGuid();
+        Email = email;
+        PasswordHash = passwordHash;
+        CreatedAt = DateTime.UtcNow;
+    }
+    
+    // Comportamiento de dominio
+    public bool VerifyPassword(string inputHash)
+    {
+        return PasswordHash == inputHash;
+    }
+}
---
+++ IAM.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using IAM.Domain.Entities;
+
+namespace IAM.Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetByEmailAsync(string email);
+    Task AddAsync(User user);
+}
